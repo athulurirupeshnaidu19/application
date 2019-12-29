7062670b60e2b79118934af1781ded7d5f04ba3f
Athuluru Rupesh <athulurirupeshnaidu19@gmail.com>
2019-12-29 11:36:42 +0530

    new recipe aded

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipes} from '../recipes.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipes[]= [
+    new Recipes('A Test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxjRAibTZqywTgxBKf3ITrTnVjRbQrdiifhiq4e3b-X9UoFthJCw&s')
+      ] ;
 
   constructor() { }
 
